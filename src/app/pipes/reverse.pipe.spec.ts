d134f89f7e5cf2f51a10c42e1744c544c4ff3485
Zelda Mazur <zelda@zeldamazur.com>
2023-11-16 18:59:48 -0600

    tested pipes

--- src/app/pipes/reverse.pipe.spec.ts
+++ src/app/pipes/reverse.pipe.spec.ts
@@ -5,4 +5,17 @@ describe('ReversePipe', () => {
     const pipe = new ReversePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it("should reverse a string", () => {
+    //arrange
+    const pipe = new ReversePipe();
+    const fwd: string = "12345";
+    const rev: string = "54321";
+
+    //act
+    const result = pipe.transform(fwd);
+    
+    //assert
+    expect(result).toBe(rev);
+  })
 });
